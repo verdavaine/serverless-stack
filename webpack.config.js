958994192c7d48b3cf30b0f4573b3c4d0618ce64
rv <paul75011@gmail.com>
2018-05-17 14:54:31 +0200

    Adding our DynamoDB resource

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,19 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
 	entry: slsw.lib.entries,
 	target: 'node',
-	// Since 'aws-sdk' is not compatible with webpack,
+	// Generate sourcemaps for proper error messages
+	devtool: 'source-map', // Since 'aws-sdk' is not compatible with webpack,
 	// we exclude all node dependencies
 	externals: [ nodeExternals() ],
+	mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+	optimization: {
+		// We no not want to minimize our code.
+		minimize: false
+	},
+	performance: {
+		// Turn off size warnings for entry points
+		hints: false
+	},
 	// Run babel on all .js files and skip those in node_modules
 	module: {
 		rules: [
